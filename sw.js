3be4378d3ddc3d209aa159455816760c3df0ac62
MotorizedWindowTreatments <matthewkandl9@gmail.com>
2026-09-01 09:02:02 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    the update instead of a stale cached copy.
    ============================================================ */
 
-const CACHE_NAME = "mwt-installer-shell-v7";
+const CACHE_NAME = "mwt-installer-shell-v8";
 
 const CORE_ASSETS = [
   "./",
